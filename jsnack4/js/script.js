d7cb4e9ed0438c5277b92076c2bf0500be1ea7fa
NicolaVerlato <nicola.verlato00@gmail.com>
2022-06-09 15:06:35 +0200

    inseriti commenti

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -10,9 +10,12 @@ for(let i = 1; i <= 6; i++) {
     // chiedere i numeri all'utente
     const userNumbers = parseInt( prompt('dammi un numero') );
 
+    // se il numero che mi darà l'tente sarà dispari,
+    // lo aggiungerò nell'array
     if(userNumbers % 2 !== 0) {
         array1.push(userNumbers);
     }
 }
 
+// risultato stampato in pagina
 console.log(array1);
